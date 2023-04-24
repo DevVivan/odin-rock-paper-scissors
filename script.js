308f5dccbe1db7d6f943851b7e23ed0b73d3c62d
Vivan <vivanpanda13@gmail.com>
2023-04-24 22:05:46 +0400

    Finish game

--- script.js
+++ script.js
@@ -21,20 +21,21 @@ function showOverlay() {
   overlay.style.display = 'block';
 }
 
+
+// Function to choose a random option from "Rock", "Paper" and "Scissors"
+
+function getComputerChoice () {
+    const options = ["rock", "paper", "scissors"];
+    let randomOption = options[Math.floor(Math.random()*options.length)];
+    return (randomOption);
+}
+
+
+
 window.onload = function() {
     let rockClick = document.getElementById('rockButton');
     let paperClick = document.getElementById('paperButton');
     let scissorsClick = document.getElementById('scissorsButton');
-
-    // Function to choose a random option from "Rock", "Paper" and "Scissors"
-
-    function getComputerChoice () {
-        const options = ["rock", "paper", "scissors"];
-        let randomOption = options[Math.floor(Math.random()*options.length)];
-        return (randomOption);
-    }
-
-    let computerSelection = getComputerChoice();
 
     // Function to get player choice
 
@@ -64,7 +65,6 @@ window.onload = function() {
             // to restart bouncing animation after delay
             userChoiceImage.setAttribute('style', 'animation: bounce 0.7s ease-in-out infinite;');
             computerChoiceImage.setAttribute('style', 'animation: bounce 0.7s ease-in-out infinite;');
-
         })
 
         paperClick.addEventListener('click', async () => {
@@ -92,7 +92,6 @@ window.onload = function() {
             // to restart bouncing animation after delay
             userChoiceImage.setAttribute('style', 'animation: bounce 0.7s ease-in-out infinite;');
             computerChoiceImage.setAttribute('style', 'animation: bounce 0.7s ease-in-out infinite;');
-
         })
 
         scissorsClick.addEventListener('click', async () => {
@@ -119,7 +118,6 @@ window.onload = function() {
             // to restart bouncing animation after delay
             userChoiceImage.setAttribute('style', 'animation: bounce 0.7s ease-in-out infinite;');
             computerChoiceImage.setAttribute('style', 'animation: bounce 0.7s ease-in-out infinite;');
-
         })
     }
     userSelection = getPlayerChoice();
@@ -127,6 +125,8 @@ window.onload = function() {
     // Function to play a single round of the game
 
     function playRound () {
+
+        let computerSelection = getComputerChoice();
 
         // if user chooses rock
 
@@ -136,9 +136,13 @@ window.onload = function() {
         } else if (userSelection == 'rock' && computerSelection == 'scissors') {
             document.getElementById("userChoiceImage").src = "images/rock.png";
             document.getElementById("computerChoiceImage").src = "images/scissors.png";
+            userScore += 1;
+            userScoreHTML.innerHTML = userScore;
         } else if (userSelection == 'rock' && computerSelection == 'paper') {
             document.getElementById("userChoiceImage").src = "images/rock.png";
             document.getElementById("computerChoiceImage").src = "images/paper.png";
+            computerScore += 1;
+            computerScoreHTML.innerHTML = computerScore;
         }
 
         // if user chooses paper
@@ -149,9 +153,13 @@ window.onload = function() {
         } else if (userSelection == 'paper' && computerSelection == 'scissors') {
             document.getElementById("userChoiceImage").src = "images/paper.png";
             document.getElementById("computerChoiceImage").src = "images/scissors.png";
+            computerScore += 1;
+            computerScoreHTML.innerHTML = computerScore;
         } else if (userSelection == 'paper' && computerSelection == 'rock') {
             document.getElementById("userChoiceImage").src = "images/paper.png";
             document.getElementById("computerChoiceImage").src = "images/rock.png";
+            userScore += 1;
+            userScoreHTML.innerHTML = userScore;
         }
 
         // if user chooses scissors
@@ -162,11 +170,14 @@ window.onload = function() {
         } else if (userSelection == 'scissors' && computerSelection == 'paper') {
             document.getElementById("userChoiceImage").src = "images/scissors.png";
             document.getElementById("computerChoiceImage").src = "images/paper.png";
+            userScore += 1;
+            userScoreHTML.innerHTML = userScore;
         } else if (userSelection == 'scissors' && computerSelection == 'rock') {
             document.getElementById("userChoiceImage").src = "images/scissors.png";
             document.getElementById("computerChoiceImage").src = "images/rock.png";
+            computerScore += 1;
+            computerScoreHTML.innerHTML = computerScore;
         }
     }
-
 };
 
